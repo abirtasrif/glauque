b23f19f6f5528875dbb81559b4563c14d7d19254
Abir Tasrif Anto <abirtasrif@gmail.com>
2024-04-04 15:37:52 +0600

    clerk auth added

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div>
+      <Button size="default" variant="outline">
+        Click Me
+      </Button>
+    </div>
+  );
+};
+
+export default Home;
